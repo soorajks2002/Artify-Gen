7ff1cf3181a95882e02b80805362873edebc1977
sooraj <soorajks2002@gmail.com>
2023-09-02 23:16:51 +0530

    fixed model loading error

--- generate_image.py
+++ generate_image.py
@@ -6,11 +6,17 @@ from PIL import Image
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
 headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.content
 
-def generate_image (prompt) :
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response
+
+
+def generate_image(prompt):
     image_bytes = query({"inputs": prompt})
-    image = Image.open(io.BytesIO(image_bytes))
-    return image
+
+    if image_bytes.status_code == 200 :
+        return Image.open(io.BytesIO(image_bytes.content))
+
+    print(image_bytes.content)
+    return Image.open("Default.jpg")

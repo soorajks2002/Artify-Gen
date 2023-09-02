bd592b08c61a28123f90ba432c3b9df1c61f8b76
sooraj <soorajks2002@gmail.com>
2023-09-02 23:16:33 +0530

    created basic streamlit ui

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import streamlit as st
 from streamlit_tags import st_tags
+from generate_prompt import generate_prompt
+from generate_image import generate_image
 
-keywords = st_tags(
-    label='# Enter Keywords:',
-    text='Press enter to add more',
-    maxtags=4)
+with st.form("main form") : 
+    keywords = st_tags(
+        label='# Enter Keywords:',
+        text='Press enter to add more',
+        maxtags=4)
+    
+    if st.form_submit_button("Generate Image Prompt") :
+        
+        with st.spinner("Generating Prompts...") :
+            prompts = generate_prompt(keywords)
+            prompts = prompts.split("\n")
+            
+            for prompt in prompts :
+                st.markdown(f"#### {prompt}")
+                
+        with st.spinner ("Generating Images...") :
+            image = generate_image("Astronaut riding a dog in antartica")
+            st.image(image)
 
-for i in keywords : 
-    st.subheader(i)

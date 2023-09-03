42fbb6e431c1517bcc501f1871cffc9d49d70d39
sooraj <soorajks2002@gmail.com>
2023-09-03 12:40:56 +0530

    updated UI - create image using the prompts generate - issue : no option to select which prompt to be used for SDXL

--- app.py
+++ app.py
@@ -2,23 +2,34 @@ import streamlit as st
 from streamlit_tags import st_tags
 from generate_prompt import generate_prompt
 from generate_image import generate_image
+from PIL import Image
+
+favicon = Image.open('favicon.ico')
+st.set_page_config(page_title='Customize Background', page_icon=favicon)
 
 with st.form("main form") : 
     keywords = st_tags(
-        label='# Enter Keywords:',
+        label='### Enter Keywords:',
         text='Press enter to add more',
         maxtags=4)
     
     if st.form_submit_button("Generate Image Prompt") :
         
-        with st.spinner("Generating Prompts...") :
-            prompts = generate_prompt(keywords)
-            prompts = prompts.split("\n")
-            
-            for prompt in prompts :
-                st.markdown(f"#### {prompt}")
+        if not keywords :
+            st.warning("Please enter keywords")
+        
+        else :
+            with st.spinner("Generating Prompts...") :
+                prompts = generate_prompt(keywords)
                 
-        with st.spinner ("Generating Images...") :
-            image = generate_image("Astronaut riding a dog in antartica")
-            st.image(image)
-
+                for i,prompt in enumerate(prompts) :
+                    st.markdown(f"#### Prompt **{i+1}** ⤵️")
+                    st.markdown(f"#### {prompt}")
+                    
+            with st.spinner ("Generating Images...") :
+                image_generation_result = generate_image(prompts[0])
+                
+                if image_generation_result["image_generated"] :
+                    st.image(image_generation_result['image'])
+                else :
+                    st.error("Hugging Face Stable Diffusion Model is BUSY !!!")

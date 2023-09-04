0cdcedb98d500c3faa1d937ccb247a1965c357c6
sooraj <soorajks2002@gmail.com>
2023-09-04 10:56:07 +0530

    streamlit ui with forms

--- app.py
+++ app.py
@@ -4,48 +4,77 @@ from generate_prompt import generate_prompt
 from generate_image import generate_image
 from PIL import Image
 
-favicon = Image.open('favicon.ico')
-st.set_page_config(page_title='Customize Background', page_icon=favicon)
+import time
 
-form = st.form("main form")
-prompt_form = st.form("prompt form")
-image_container = st.container()
+favicon = Image.open('favicon.ico')
+st.set_page_config(page_title='Customize Background',
+                   page_icon=favicon, layout='wide')
+
+col1, col2 = st.columns(2)
+with col1:
+    keywords_form = st.form('keyword form')
+    prompts_form = st.form('prompt form')
+with col2:
+    images_form = st.form('Stable Diffusion Image Generation')
 
 form_submitted = None
 prompt_form_submitted = None
+st.session_state['generated_prompts'] = []
 
 prompt_list = ["Prompt 1", "Prompt 2", "Prompt 3"]
 
-with form : 
+def image_form_block():
+    
+    with images_form:
+        with st.spinner("Generating Images..."):
+
+            prompts = st.session_state['generated_prompts']
+            prompt_no = st.session_state['prompt-radio']
+            prompt = prompts[prompt_list.index(prompt_no)]
+            st.write(prompt)
+
+            image_generation_result = generate_image(prompt)
+
+            if image_generation_result['image_generated'] :
+                st.image(image_generation_result['image'])
+            else :
+                st.error("Hugging Face Stable Diffusion Model is BUSY !!!")
+        
+        vlue = st.form_submit_button("UNCLIKABLE BUTTON", disabled=True)
+
+
+def prompt_form_block():
+    
+    with prompts_form:
+        with st.spinner("Generating Prompts..."):
+            keywords = st.session_state['prompt-keywords']
+
+            st.session_state['generated_prompts'] = generate_prompt(keywords)
+
+        prompts = st.session_state['generated_prompts']
+        
+        for i, prompt in enumerate(prompts):
+            st.markdown(f"#### Prompt **{i+1}** ⤵️")
+            st.markdown(f"##### {prompt}")
+
+        prompt_radio_button = st.radio(
+            "# Select Prompt ..", prompt_list, key='prompt-radio')
+        prompt_form_submitted = st.form_submit_button(
+            "Generate Image", on_click=image_form_block)
+
+
+with keywords_form:
     keywords = st_tags(
         label='### Enter Keywords:',
         text='Press enter to add more',
-        maxtags=4)
-    
-    form_submitted = st.form_submit_button("Generate Image Prompt") 
+        maxtags=4, key='prompt-keywords')
 
-if form_submitted :        
-    if not keywords :
-        st.warning("Please enter keywords")
-    
-    else :
-        with st.spinner("Generating Prompts...") :
-            prompts = generate_prompt(keywords)
-            
-            with prompt_form :
-                for i,prompt in enumerate(prompts) :
-                    st.markdown(f"#### Prompt **{i+1}** ⤵️")
-                    st.markdown(f"##### {prompt}")
-                    
-                prompt = st.radio("Select Prompt ..", prompt_list)
-                prompt_form_submitted = st.form_submit_button("Generate Prompt")
-                
-if prompt_form_submitted :   
-    with st.spinner ("Generating Images...") :
-        image_generation_result = generate_image(prompt_list.index(prompt))
-        
-        if image_generation_result["image_generated"] :
-            
-            image_container.image(image_generation_result['image'])
-        else :
-            image_container.error("Hugging Face Stable Diffusion Model is BUSY !!!")
+    form_submitted = st.form_submit_button(
+        "Generate Image Prompt")
+
+    if form_submitted:
+        if not keywords:
+            st.warning("Please enter keywords")
+
+        else:
+            prompt_form_block()
